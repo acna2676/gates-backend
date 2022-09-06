70012bbc0ee6187cc50afb3f35b2e3c540f2eff5
ryosuke nakaishi <acna2676@gmail.com>
2022-09-06 23:28:37 +0900

    note: create apig stack

--- create_pipeline_stack.sh
+++ create_pipeline_stack.sh
@@ -1,2 +1,3 @@
 aws cloudformation create-stack --stack-name  gates-dev-codepipeline --template-body file://codepipeline.yaml --capabilities CAPABILITY_IAM
+aws cloudformation update-stack --stack-name  gates-dev-codepipeline --template-body file://codepipeline.yaml --capabilities CAPABILITY_IAM
 
